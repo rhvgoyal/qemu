ca68c714044389e37140b77b0d2b5e7f8f634a6b
Dr. David Alan Gilbert <dgilbert@redhat.com>
2018-07-27 10:36:41 +0100

    virtio-fs: Allow mapping of meta data version table
    The 'meta data version table' is a block of shared memory mapped between
    multiple QEMUs and fuse daemons, so that they can be informed
    of metadata updates.  It's typically a shmfs file, and
    it's specified as :
    
       -device vhost-user-fs-pci,chardev=char0,tag=myfs,cache-size=1G,versiontable=/dev/shm/mdvt1
    
    It gets mapped into the PCI bar after the data cache; it's read only.
    
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- include/hw/virtio/vhost-user-fs.h
+++ include/hw/virtio/vhost-user-fs.h
@@ -47,6 +47,7 @@ typedef struct {
     uint16_t num_request_queues;
     uint16_t queue_size;
     uint64_t cache_size;
+    char *mdvtpath;
 } VHostUserFSConf;
 
 typedef struct {
@@ -61,6 +62,9 @@ typedef struct {
 
     /*< public >*/
     MemoryRegion cache;
+    /* Metadata version table */
+    size_t mdvt_size;
+    MemoryRegion mdvt;
 } VHostUserFS;
 
 /* Callbacks from the vhost-user code for slave commands */
