8b98ca9e72621981f0b2f90ae93e099dc36870e3
Miklos Szeredi <mszeredi@redhat.com>
2018-11-15 15:29:51 +0100

    virtiofsd: passthrough_ll: use hashtable
    Improve performance of inode lookup by using a hash table.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Signed-off-by: Liu Bo <bo.liu@linux.alibaba.com>

--- contrib/virtiofsd/passthrough_ll.c
+++ contrib/virtiofsd/passthrough_ll.c
@@ -69,6 +69,7 @@
 #include "ireg.h"
 
 #include <glib.h>
+#include <gmodule.h>
 #include "passthrough_helpers.h"
 #include "seccomp.h"
 
@@ -91,13 +92,15 @@ struct lo_map {
 	ssize_t freelist;
 };
 
-struct lo_inode {
-	struct lo_inode *next; /* protected by lo->mutex */
-	struct lo_inode *prev; /* protected by lo->mutex */
-	int fd;
-	bool is_symlink;
+struct lo_key {
 	ino_t ino;
 	dev_t dev;
+};
+
+struct lo_inode {
+	int fd;
+	bool is_symlink;
+	struct lo_key key;
 	uint64_t refcount; /* protected by lo->mutex */
 	uint64_t version_offset;
 	uint64_t ireg_refid;
@@ -132,7 +135,8 @@ struct lo_data {
 	int ireg_sock;
 	int64_t *version_table;
 	uint64_t version_table_size;
-	struct lo_inode root; /* protected by lo->mutex */
+	struct lo_inode root;
+	GHashTable *inodes; /* protected by lo->mutex */
 	struct lo_map ino_map; /* protected by lo->mutex */
 	struct lo_map dirp_map; /* protected by lo->mutex */
 	struct lo_map fd_map; /* protected by lo->mutex */
@@ -593,7 +597,7 @@ retry:
 			fuse_log(FUSE_LOG_WARNING, "lo_parent_and_name: failed to stat last\n");
 		goto fail_unref;
 	}
-	if (stat.st_dev != inode->dev || stat.st_ino != inode->ino) {
+	if (stat.st_dev != inode->key.dev || stat.st_ino != inode->key.ino) {
 		if (!retries)
 			fuse_log(FUSE_LOG_WARNING, "lo_parent_and_name: failed to match last\n");
 		goto fail_unref;
@@ -766,19 +770,20 @@ out_err:
 static struct lo_inode *lo_find(struct lo_data *lo, struct stat *st)
 {
 	struct lo_inode *p;
-	struct lo_inode *ret = NULL;
+	struct lo_key key = {
+		.ino = st->st_ino,
+		.dev = st->st_dev,
+	};
 
 	pthread_mutex_lock(&lo->mutex);
-	for (p = lo->root.next; p != &lo->root; p = p->next) {
-		if (p->ino == st->st_ino && p->dev == st->st_dev) {
-			assert(p->refcount > 0);
-			ret = p;
-			ret->refcount++;
-			break;
-		}
+	p = g_hash_table_lookup(lo->inodes, &key);
+	if (p) {
+		assert(p->refcount > 0);
+		p->refcount++;
 	}
 	pthread_mutex_unlock(&lo->mutex);
-	return ret;
+
+	return p;
 }
 
 struct msgreply {
@@ -796,8 +801,8 @@ static void get_shared(struct lo_data *lo, struct lo_inode *inode)
 		.op = IREG_GET,
 		.handle = (uintptr_t) &rep,
 		.get = {
-			.ino = inode->ino,
-			.dev = inode->dev,
+			.ino = inode->key.ino,
+			.dev = inode->key.dev,
 		},
 	};
 
@@ -881,8 +886,6 @@ static int lo_do_lookup(fuse_req_t req, fuse_ino_t parent, const char *name,
 		close(newfd);
 		newfd = -1;
 	} else {
-		struct lo_inode *prev, *next;
-
 		saverr = ENOMEM;
 		inode = calloc(1, sizeof(struct lo_inode));
 		if (!inode)
@@ -892,19 +895,14 @@ static int lo_do_lookup(fuse_req_t req, fuse_ino_t parent, const char *name,
 		inode->refcount = 1;
 		inode->fd = newfd;
 		newfd = -1;
-		inode->ino = e->attr.st_ino;
-		inode->dev = e->attr.st_dev;
+		inode->key.ino = e->attr.st_ino;
+		inode->key.dev = e->attr.st_dev;
 
 		get_shared(lo, inode);
 
 		pthread_mutex_lock(&lo->mutex);
 		inode->fuse_ino = lo_add_inode_mapping(req, inode);
-		prev = &lo->root;
-		next = prev->next;
-		next->prev = inode;
-		inode->next = next;
-		inode->prev = prev;
-		prev->next = inode;
+                g_hash_table_insert(lo->inodes, &inode->key, inode);
 		pthread_mutex_unlock(&lo->mutex);
 	}
 
@@ -1292,14 +1290,8 @@ static void unref_inode_lolocked(struct lo_data *lo, struct lo_inode *inode, uin
 	assert(inode->refcount >= n);
 	inode->refcount -= n;
 	if (!inode->refcount) {
-		struct lo_inode *prev, *next;
-
-		prev = inode->prev;
-		next = inode->next;
-		next->prev = prev;
-		prev->next = next;
-
 		lo_map_remove(&lo->ino_map, inode->fuse_ino);
+                g_hash_table_remove(lo->inodes, &inode->key);
 		pthread_mutex_unlock(&lo->mutex);
 		close(inode->fd);
 		put_shared(lo, inode);
@@ -1486,7 +1478,7 @@ static void lo_do_readdir(fuse_req_t req, fuse_ino_t ino, size_t size,
 
 		/* Hide root's parent directory */
 		if (dinode == &lo->root && strcmp(name, "..") == 0) {
-			e.attr.st_ino = lo->root.ino;
+			e.attr.st_ino = lo->root.key.ino;
 			e.attr.st_mode = DT_DIR << 12;
 		}
 
@@ -2500,9 +2492,29 @@ static void setup_root(struct lo_data *lo, struct lo_inode *root)
 	}
 
 	root->fd = fd;
-	root->ino = stat.st_ino;
-	root->dev = stat.st_dev;
+	root->key.ino = stat.st_ino;
+	root->key.dev = stat.st_dev;
 	root->refcount = 2;
+}
+
+static guint lo_key_hash(gconstpointer key)
+{
+	const struct lo_key *lkey = key;
+	guint res;
+
+	res = g_direct_hash(GINT_TO_POINTER(lkey->ino));
+	res += g_direct_hash(GINT_TO_POINTER(lkey->dev));
+
+	return res;
+}
+
+static gboolean lo_key_equal(gconstpointer a, gconstpointer b)
+{
+	const struct lo_key *la = a;
+	const struct lo_key *lb = b;
+
+	return la->ino == lb->ino &&
+	       la->dev == lb->dev;
 }
 
 int main(int argc, char *argv[])
@@ -2523,7 +2535,7 @@ int main(int argc, char *argv[])
 	setup_nofile_rlimit();
 
 	pthread_mutex_init(&lo.mutex, NULL);
-	lo.root.next = lo.root.prev = &lo.root;
+	lo.inodes = g_hash_table_new(lo_key_hash, lo_key_equal);
 	lo.root.fd = -1;
 	lo.root.fuse_ino = FUSE_ROOT_ID;
 	lo.cache = CACHE_AUTO;
@@ -2663,6 +2675,8 @@ err_out2:
 err_out1:
 	fuse_opt_free_args(&args);
 
+        if (lo.inodes)
+                g_hash_table_destroy(lo.inodes);
 	lo_map_destroy(&lo.fd_map);
 	lo_map_destroy(&lo.dirp_map);
 	lo_map_destroy(&lo.ino_map);
