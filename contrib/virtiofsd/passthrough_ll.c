6810fb9fa5a1d421a007b6442113a9373df73122
Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
2019-04-17 03:08:58 +0800

    virtiofsd: use fallocate(2) instead posix_fallocate(3)
    This is because posix_fallocate(3) does not support FALLOC_FL_KEEP_SIZE
    and FALLOC_FL_PUNCH_HOLE. Our underlying host filesystem is ext4 and
    ext4 supports FALLOC_FL_KEEP_SIZE and FALLOC_FL_PUNCH_HOLE well, so
    this change will be ok.
    
    Signed-off-by: Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
    Reviewed-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- contrib/virtiofsd/passthrough_ll.c
+++ contrib/virtiofsd/passthrough_ll.c
@@ -1568,15 +1568,10 @@ static void lo_fallocate(fuse_req_t req, fuse_ino_t ino, int mode,
 			 off_t offset, off_t length, struct fuse_file_info *fi)
 {
 	int err;
-	(void) ino;
 
-	if (mode) {
-		fuse_reply_err(req, EOPNOTSUPP);
-		return;
-	}
-
-	err = posix_fallocate(lo_fi_fd(req, fi), offset,
-			      length);
+	err = fallocate(lo_fi_fd(req, fi), mode, offset, length);
+        if (err < 0)
+                err = errno;
 
 	fuse_reply_err(req, err);
 }
