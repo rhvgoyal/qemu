nabla-containers/qemu-virtiofs

virtiofsd: passthrough_ll: disable readdirplus on cache=never
...because the attributes sent in the READDIRPLUS reply would be discarded
anyway.

Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

contrib/virtiofsd/passthrough_ll.c
@@ -172,6 +172,12 @@ static void lo_init(void *userdata,
 			fprintf(stderr, "lo_init: activating flock locks\n");
 		conn->want |= FUSE_CAP_FLOCK_LOCKS;
 	}
+	if (lo->cache == CACHE_NEVER) {
+		if (lo->debug)
+			fprintf(stderr, "lo_init: disabling readdirplus\n");
+		conn->want &= ~FUSE_CAP_READDIRPLUS;
+	}
+
 }
 
 static void lo_getattr(fuse_req_t req, fuse_ino_t ino,

rhvgoyal/qemu

contrib/virtiofsd/passthrough_ll.c
@@ -468,6 +468,11 @@ static void lo_init(void *userdata,
 		fuse_log(FUSE_LOG_DEBUG, "lo_init: activating flock locks\n");
 		conn->want |= FUSE_CAP_FLOCK_LOCKS;
 	}
+	if (lo->cache == CACHE_NEVER) {
+		fuse_log(FUSE_LOG_DEBUG, "lo_init: disabling readdirplus\n");
+		conn->want &= ~FUSE_CAP_READDIRPLUS;
+	}
+
 }
 
 static void lo_getattr(fuse_req_t req, fuse_ino_t ino,