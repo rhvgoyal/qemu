ecc6e6154c0fab9b9a96f911d50905b6c97d6bf7
Vivek Goyal <vgoyal@redhat.com>
2020-07-30 15:39:02 -0400

    virtiofsd: Open lo->source while setting up root in sandbox=NONE mode
    In sandbox=NONE mode, lo->source points to the directory which is being
    exported. We have not done any chroot()/pivot_root(). So open lo->source.
     
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>

--- tools/virtiofsd/passthrough_ll.c
+++ tools/virtiofsd/passthrough_ll.c
@@ -3209,7 +3209,10 @@ static void setup_root(struct lo_data *lo, struct lo_inode *root)
     int fd, res;
     struct stat stat;
 
-    fd = open("/", O_PATH);
+    if (lo->sandbox == SANDBOX_NONE)
+        fd = open(lo->source, O_PATH);
+    else
+        fd = open("/", O_PATH);
     if (fd == -1) {
         fuse_log(FUSE_LOG_ERR, "open(%s, O_PATH): %m\n", lo->source);
         exit(1);
