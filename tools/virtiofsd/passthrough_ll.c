e840e8310d7846abe662257629dabae73c8b7201
Vivek Goyal <vgoyal@redhat.com>
2020-12-08 12:56:55 -0500

    virtiofsd: Save error code early at the failure callsite
    Change error code handling slightly in lo_setattr(). Right now we seem
    to jump to out_err and assume that "errno" is valid and use that to
    send reply.
    
    But if caller has to do some other operations before jumping to out_err,
    then it does the dance of first saving errno to saverr and the restore
    errno before jumping to out_err. This makes it more confusing. 
    
    I am about to make more changes where caller will have to do some
    work after error before jumping to out_err. I found it easier to
    change the convention a bit. That is caller saves error in "saverr"
    before jumping to out_err. And out_err uses "saverr" to send error
    back and does not rely on "errno" having actual error.
    
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>

--- tools/virtiofsd/passthrough_ll.c
+++ tools/virtiofsd/passthrough_ll.c
@@ -678,6 +678,7 @@ static void lo_setattr(fuse_req_t req, fuse_ino_t ino, struct stat *attr,
             res = fchmodat(lo->proc_self_fd, procname, attr->st_mode, 0);
         }
         if (res == -1) {
+            saverr = errno;
             goto out_err;
         }
     }
@@ -687,6 +688,7 @@ static void lo_setattr(fuse_req_t req, fuse_ino_t ino, struct stat *attr,
 
         res = fchownat(ifd, "", uid, gid, AT_EMPTY_PATH | AT_SYMLINK_NOFOLLOW);
         if (res == -1) {
+            saverr = errno;
             goto out_err;
         }
     }
@@ -699,15 +701,15 @@ static void lo_setattr(fuse_req_t req, fuse_ino_t ino, struct stat *attr,
             sprintf(procname, "%i", ifd);
             truncfd = openat(lo->proc_self_fd, procname, O_RDWR);
             if (truncfd < 0) {
+                saverr = errno;
                 goto out_err;
             }
         }
 
         res = ftruncate(truncfd, attr->st_size);
+        saverr = res == -1 ? errno : 0;
         if (!fi) {
-            saverr = errno;
             close(truncfd);
-            errno = saverr;
         }
         if (res == -1) {
             goto out_err;
@@ -740,6 +742,7 @@ static void lo_setattr(fuse_req_t req, fuse_ino_t ino, struct stat *attr,
             res = utimensat(lo->proc_self_fd, procname, tv, 0);
         }
         if (res == -1) {
+            saverr = errno;
             goto out_err;
         }
     }
@@ -748,7 +751,6 @@ static void lo_setattr(fuse_req_t req, fuse_ino_t ino, struct stat *attr,
     return lo_getattr(req, ino, fi);
 
 out_err:
-    saverr = errno;
     lo_inode_put(lo, &inode);
     fuse_reply_err(req, saverr);
 }
